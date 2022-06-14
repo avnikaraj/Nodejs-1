2b4e9a760b51a344b08f8719ca91b0ce4b1d560d
avnikaraj <akaraj@unitsystem.net>
2022-06-14 11:34:10 +0200

    Async function

--- app/index.js
+++ app/index.js
@@ -1,15 +1,27 @@
-const fs = require("fs");
 const _ = require("lodash");
 
 
-function read(path) {
+// function read(path) {
+//   try {
+//     const data = fs.readFileSync("app/data.json", { encoding: "utf8" });
+//     return JSON.parse(data);
+//   } catch (err) {
+//     return [];
+//   }
+// }
+
+
+const fs = require('fs/promises');
+
+async function read() {
   try {
-    const data = fs.readFileSync("app/data.json", { encoding: "utf8" });
+    const data = await fs.readFile('app/data.json', { encoding: 'utf8' });
     return JSON.parse(data);
   } catch (err) {
     return [];
   }
 }
+
 
 
 module.exports.read = read
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { sum } = require('./app/calc');
 const { concat } = require('./app/concat');
 const { read }  = require('./app/index');
-const data = read("./app/data.json");
 
-console.log("Hello world")
 
-console.log(`The result of the sum is ${sum(data)}`);
-console.log(`The result of the concat is ${concat(data)}`);
-console.log(`The result of the sum is ${sum(concat(data))}`);
+read("./app/data.json").then(data=> {
+    console.log(`The result of the sum is ${sum(data)}`);
+    console.log(`The result of the concat is ${concat(data)}`);
+    console.log(`The result of the sum is ${sum(concat(data))}`);
+});
+
+// console.log("Hello world")
